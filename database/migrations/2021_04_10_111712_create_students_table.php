bc3e7a39174b60e1052501cc6b865d4911bdec33
jhoncarlo-condy <jhoncarlo.condy@gmail.com>
2021-04-10 19:22:47 +0800

    Add student Model,Controller and Migration

---
+++ database/migrations/2021_04_10_111712_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('stud_id')->unique();
+            $table->int('name_id');
+            $table->int('age');
+            $table->int('grade_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
